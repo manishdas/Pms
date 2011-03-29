11e05ab27eed7afb76b09b866b2eb1dcd520f1cf
Manish <manish.das@sprout-technology.com>
2011-03-29 13:35:50 +0545

    initial structure of Project management system with devise authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pms::Application.config.secret_token = 'eaf1777bf966dfc96a6325041ce7c78e34fd25cab62e82fdc6b874572f4c9cb01495fcb468a3ce8643bf7328c99a62e630026ccffd043f05bff8d621092dbd18'
